6a67769dab247521bcf29f4444fe70dc4ad792f2
Manthan <66257636+Manthanp18@users.noreply.github.com>
2021-03-18 09:27:36 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-//  import EditBlog from "./components/edit";
+import EditBlog from "./components/edit";
 import CreateBlog from "./components/create";
-//  import BlogList from "./components/bloglist";
+import BlogList from "./components/bloglist";
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <div className="container">
       <Navbar />
       <br/>
-      {/* <Route path="/blogs" component={BlogList} />
-      <Route path="/api/blogs:id" component={EditBlog} /> */}
-      <Route path="/api/blogs" component={CreateBlog} />
+      <Route path="/api/blogs" component={BlogList} />
+      <Route path="/api/blog/edit/:id" component={EditBlog} />
+      <Route path="/api/blog/add" component={CreateBlog} />
       </div>
     </Router>
   );
